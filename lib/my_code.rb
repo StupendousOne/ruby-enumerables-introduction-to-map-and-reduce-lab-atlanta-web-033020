ed7ce9da246eb07fb0dc38ee31a32186e29bc9d9
Geoffrey Harrison <harrisongeo87@gmail.com>
2020-03-27 00:11:01 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,4 +1,42 @@
 # My Code here....
-def map_to_negativize(array) do
+def map_to_negativize(array)
+  new_array = []
+  count = 0
   
+  while array[count] do
+    new_array << 
+    count += 1
+  end
+  
+  new_array
+end
+
+def map_to_no_change(array)
+  new_array = []
+  count = 0
+  
+   while array[count] do
+    new_array << 
+    count += 1
+  end
+  
+  new_array
+end
+
+def map_to_double(array)
+  new_array = []
+  count = 0
+  
+  
+  
+  new_array
+end
+
+def map_to_square(array)
+  new_array = []
+  count = 0
+  
+  
+  
+  new_array
 end
